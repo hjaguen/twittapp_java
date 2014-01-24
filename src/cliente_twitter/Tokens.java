fdd05c33212524c463ec3cb5e6f0e0c960ec65ec
Andres Mauricio Ardila Marin <hjaguen@gmail.com>
2014-01-23 21:48:01 -0500

    Casi Terminado, falta que funcione, nada más

---
+++ src/cliente_twitter/Tokens.java
@@ -0,0 +1,12 @@
+package cliente_twitter;
+
+/**
+*
+* @author Adela
+*/
+public class Tokens {
+
+    String OAuthConsumerKey = "XXXXXXXXXXXXXXXXXXXX";
+    String OAuthConsumerSecret = "XXXXXXXXXXXXXXXXXXXX";
+
+}
